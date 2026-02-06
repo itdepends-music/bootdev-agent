c9447022c2a017d30ee8a8397e550521fc0069f0
Oscar Robertson <itdependsmusic@proton.me>
2026-02-07 09:34:25 +1300

    refactor to allow for multiple messages in future and add verbose mode

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 from prompts import system_prompt
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 load_dotenv()
@@ -17,13 +19,16 @@ if not api_key:
 
 client = genai.Client(api_key=api_key)
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 content = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt,
-    config=genai.types.GenerateContentConfig(system_instruction=system_prompt),  # type: ignore
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
-print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")  # type: ignore
-print(f"Response tokens: {content.usage_metadata.candidates_token_count}")  # type: ignore
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")  # type: ignore
+    print(f"Response tokens: {content.usage_metadata.candidates_token_count}")  # type: ignore
 
 print(content.text)
